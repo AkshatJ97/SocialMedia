59a5649333e6d7532ae1aa4e51a92e2c7000014d
AkshatJ97 <66557762+AkshatJ97@users.noreply.github.com>
2025-01-12 15:44:53 -0500

    Delete social-media-project directory

--- social-media-project/src/main/java/com/demo/social_media_project/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.demo.social_media_project.config;
-
-public class JwtConstant {
-
-    public static String JWT_HEADER = "Authorization";
-    public static String SECRET_KEY = "kcnsdlkvnsdlvknkshjgbeiueiofhwefoihwefoihwefoiwhfoweihowefihweofihwfosdlvkn";
-}
--- social-media-project/src/main/java/com/demo/social_media_project/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.social_media_project.repository;
-
-import com.demo.social_media_project.models.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Integer> {
-}
